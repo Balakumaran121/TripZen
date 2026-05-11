1fcfe2f5b10a6ad48f857f6f64d3289a37874647
Balakumaran <bkumaran121@gmail.com>
2026-05-11 09:42:53 +0530

    Added the pages and navamenus

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,6 +4,10 @@ import Orders from './pages/Orders';
 import Login from './pages/Login';
 import CommonLayout from './layouts/CommonLayout';
 import Errorpage from './pages/Errorpage';
+import Trips from './pages/Trips';
+import Vehicle from './pages/Vehicle';
+import Settings from './pages/Settings';
+import Customer from './pages/Customer';
 
 const AppRouter = () => {
    return (
@@ -13,6 +17,10 @@ const AppRouter = () => {
          <Route path="/" element={<CommonLayout />}>
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/orders" element={<Orders />} />
+            <Route path="/trips" element={<Trips />} />
+            <Route path="/vehicle" element={<Vehicle />} />
+            <Route path="/customer" element={<Customer />} />
+            <Route path="/settings" element={<Settings />} />
          </Route>
          <Route path="*" element={<Errorpage />} />
       </Routes>
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,17 +1,52 @@
+import { Link, useNavigate } from 'react-router-dom';
+
 const Navbar = () => {
+   const navigate = useNavigate();
+   const NAV_ITEMS = [
+      {
+         id: 1,
+         name: 'Dashboard',
+      },
+      {
+         id: 2,
+         name: 'Orders',
+      },
+      {
+         id: 3,
+         name: 'Trips',
+      },
+      {
+         id: 4,
+         name: 'Vehicle',
+      },
+      {
+         id: 5,
+         name: 'Customer',
+      },
+      {
+         id: 6,
+         name: 'Settings',
+      },
+   ];
+   const handleLogout = () => {
+      navigate('/login');
+   };
    return (
       <div className="bg-gray-800 text-white space-y-4 h-[91.7dvh] w-40 flex flex-col justify-between p-2">
          <div>
             <ul className="space-y-4 p-2">
-               <li>Dashboard</li>
-               <li>Trips</li>
-               <li>Vehicle</li>
-               <li>Customer</li>
-               <li>Settings</li>
+               {NAV_ITEMS.map((item) => (
+                  <li key={item.id}>
+                     <Link to={`/${item.name.toLowerCase()}`}>{item.name}</Link>
+                  </li>
+               ))}
             </ul>
          </div>
          <div>
-            <button className="bg-red-500 hover:bg-red-700 text-white rounded w-full h-10 hover:cursor-pointer text-lg font-medium">
+            <button
+               onClick={handleLogout}
+               className="bg-red-500 hover:bg-red-700 text-white rounded w-full h-10 hover:cursor-pointer text-lg font-medium"
+            >
                Logout
             </button>
          </div>
